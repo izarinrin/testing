17dffbac46be9a45e5ff0339525973babffe7e65
M.Demabildo <>
2021-08-17 16:18:08 +0800

    does this still work?

---
+++ src/prctise/Prctise.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prctise;
+
+/**
+ *
+ * @author PerlaMay
+ */
+public class Prctise {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+      int a = 0;
+      
+        for (int i = 0; i < 10; i++) {
+           System.out.println("amaw si may"+a++); 
+        }      
+    }
+    
+}
